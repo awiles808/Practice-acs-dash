884460107ff0360b263a4748a893774b1f12d908
vulpes37 <rjlevitt37@gmail.com>
2020-08-28 19:28:21 -0600

    Deleted macho man, added mapbox map

---
+++ frontend-sandbox/js/mapbox.js
@@ -0,0 +1,10 @@
+
+mapboxgl.accessToken = 'pk.eyJ1IjoidnVscGVzMzciLCJhIjoiY2ptcDMwYXRhMHRzYTNrbHVscjE5Y3QwbSJ9.5DWPIW1ECOxfixUZ5JVylg';
+
+// mapbox dictionary
+var map = new mapboxgl.Map({
+container: 'map', // container id
+style: 'mapbox://styles/vulpes37/ckeeylqg10cox19rykiptnnjc', // style URL
+center: [-105.03, 40.50], // starting position [lng, lat]
+zoom: 9 // starting zoom
+});
